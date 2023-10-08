be02a82fb8f85dc62d125bdb4d286011ef7a34bf
itaruby_on_rails <itaruo93o@icloud.com>
2023-10-09 08:47:00 +0900

    make seeds file, serializer, show action

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   namespace :api do
     namespace :v1 do
       get 'health_check', to: 'health_check#index'
       mount_devise_token_auth_for 'User', at: 'auth'
+      mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
       namespace :current do
         resource :user, only: [:show]
       end
+      resources :articles, only: [:show]
     end
   end
 end
