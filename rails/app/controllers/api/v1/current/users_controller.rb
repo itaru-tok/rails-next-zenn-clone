4ad7ab776c1ae521ddd842768b5bcf6b5b8e1f8f
itaruby_on_rails <itaruo93o@icloud.com>
2023-10-09 08:17:31 +0900

    run rubocop, lint

--- rails/app/controllers/api/v1/current/users_controller.rb
+++ rails/app/controllers/api/v1/current/users_controller.rb
@@ -1,7 +1,15 @@
-class Api::V1::Current::UsersController < Api::V1::BaseController
-  before_action :authenticate_user!
+# frozen_string_literal: true
 
-  def show
-    render json: current_user, serializer: CurrentUserSerializer
+module Api
+  module V1
+    module Current
+      class UsersController < Api::V1::BaseController
+        before_action :authenticate_user!
+
+        def show
+          render json: current_user, serializer: CurrentUserSerializer
+        end
+      end
+    end
   end
 end
