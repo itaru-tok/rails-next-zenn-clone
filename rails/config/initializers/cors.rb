4d54023b334ddaced51f4c3813e0b86bfc78cf8a
itaruby_on_rails <itaruo93o@icloud.com>
2023-10-08 12:02:13 +0900

    correct by rubocop, lint

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -9,8 +11,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins Settings.front_domain
 
-    resource "*",
+    resource '*',
              headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             methods: %i[get post put patch delete options head]
   end
 end
