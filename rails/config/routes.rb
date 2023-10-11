c83f78c1ea51351be84ca77b4c5dc9d87036b7f5
itaruby_on_rails <itaruo93o@icloud.com>
2023-10-11 10:49:16 +0900

    get index, show of my articles

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       namespace :current do
         resource :user, only: [:show]
         resources :articles, only: [:create]
-        resources :articles, only: [:create, :update]
+        resources :articles, only: [:index, :show, :create, :update]
       end
       resources :articles, only: %i[index show]
     end
