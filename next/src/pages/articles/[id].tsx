f7a26f5de58e81949d7db9751ca6027f179e5001
itaruby_on_rails <itaruo93o@icloud.com>
2023-10-14 11:53:24 +0900

    make lint.correct

--- next/src/pages/articles/[id].tsx
+++ next/src/pages/articles/[id].tsx
@@ -1,6 +1,6 @@
-import ArticleIcon from "@mui/icons-material/Article";
-import PersonIcon from "@mui/icons-material/Person";
-import UpdateIcon from "@mui/icons-material/Update";
+import ArticleIcon from '@mui/icons-material/Article'
+import PersonIcon from '@mui/icons-material/Person'
+import UpdateIcon from '@mui/icons-material/Update'
 import {
   Box,
   Container,
@@ -9,54 +9,54 @@ import {
   List,
   ListItem,
   ListItemText,
-} from "@mui/material";
-import camelcaseKeys from "camelcase-keys";
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import useSWR from "swr";
-import Error from "@/components/Error";
-import Loading from "@/components/Loading";
-import MarkdownText from "@/components/MarkdownText";
-import { fetcher } from "@/utils";
+} from '@mui/material'
+import camelcaseKeys from 'camelcase-keys'
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+import Error from '@/components/Error'
+import Loading from '@/components/Loading'
+import MarkdownText from '@/components/MarkdownText'
+import { fetcher } from '@/utils'
 
 type ArticleProps = {
-  title: string;
-  content: string;
-  createdAt: string;
-  updatedAt: string;
+  title: string
+  content: string
+  createdAt: string
+  updatedAt: string
   user: {
-    name: string;
-  };
-};
+    name: string
+  }
+}
 
 const ArticleDetail: NextPage = () => {
-  const router = useRouter();
-  const url = process.env.NEXT_PUBLIC_API_BASE_URL + "/articles/";
-  const { id } = router.query;
+  const router = useRouter()
+  const url = process.env.NEXT_PUBLIC_API_BASE_URL + '/articles/'
+  const { id } = router.query
 
-  const { data, error } = useSWR(id ? url + id : null, fetcher);
-  if (error) return <Error />;
-  if (!data) return <Loading />;
+  const { data, error } = useSWR(id ? url + id : null, fetcher)
+  if (error) return <Error />
+  if (!data) return <Loading />
 
-  const article: ArticleProps = camelcaseKeys(data);
+  const article: ArticleProps = camelcaseKeys(data)
 
   return (
     <Box
       sx={{
-        backgroundColor: "#EDF2F7",
+        backgroundColor: '#EDF2F7',
         pb: 6,
-        minHeight: "calc(100vh - 57px)",
+        minHeight: 'calc(100vh - 57px)',
       }}
     >
       <Box
         sx={{
-          display: { xs: "flex", lg: "none" },
-          alignItems: "center",
-          backgroundColor: "white",
-          borderTop: "0.5px solid #acbcc7",
+          display: { xs: 'flex', lg: 'none' },
+          alignItems: 'center',
+          backgroundColor: 'white',
+          borderTop: '0.5px solid #acbcc7',
           height: 56,
           pl: 4,
-          color: "#6e7b85",
+          color: '#6e7b85',
         }}
       >
         <Box sx={{ pr: 1 }}>
@@ -65,18 +65,18 @@ const ArticleDetail: NextPage = () => {
         <Box sx={{ mr: 2 }}>
           <Typography component="p">著者:</Typography>
         </Box>
-        <Typography component="p" sx={{ fontWeight: "bold", color: "black" }}>
+        <Typography component="p" sx={{ fontWeight: 'bold', color: 'black' }}>
           {article.user.name}
         </Typography>
       </Box>
       <Container maxWidth="lg">
         <Box sx={{ pt: 6, pb: 3 }}>
-          <Box sx={{ maxWidth: 840, m: "auto", textAlign: "center" }}>
+          <Box sx={{ maxWidth: 840, m: 'auto', textAlign: 'center' }}>
             <Typography
               component="h2"
               sx={{
                 fontSize: { xs: 21, sm: 25 },
-                fontWeight: "bold",
+                fontWeight: 'bold',
               }}
             >
               {article.title}
@@ -87,30 +87,30 @@ const ArticleDetail: NextPage = () => {
             align="center"
             sx={{
               display: {
-                xs: "block",
-                lg: "none",
+                xs: 'block',
+                lg: 'none',
               },
-              color: "#6e7b85",
-              mt: "20px",
+              color: '#6e7b85',
+              mt: '20px',
             }}
           >
             {article.createdAt}に公開
           </Typography>
         </Box>
-        <Box sx={{ display: "flex", gap: "0 24px" }}>
-          <Box sx={{ width: "100%" }}>
+        <Box sx={{ display: 'flex', gap: '0 24px' }}>
+          <Box sx={{ width: '100%' }}>
             <Card
               sx={{
-                boxShadow: "none",
-                borderRadius: "12px",
+                boxShadow: 'none',
+                borderRadius: '12px',
                 maxWidth: 840,
-                m: "0 auto",
+                m: '0 auto',
               }}
             >
               <Box
                 sx={{
-                  padding: { xs: "0 24px 24px 24px", sm: "0 40px 40px 40px" },
-                  marginTop: { xs: "24px", sm: "40px" },
+                  padding: { xs: '0 24px 24px 24px', sm: '0 40px 40px 40px' },
+                  marginTop: { xs: '24px', sm: '40px' },
                 }}
               >
                 <MarkdownText content={article.content} />
@@ -119,23 +119,23 @@ const ArticleDetail: NextPage = () => {
           </Box>
           <Box
             sx={{
-              display: { xs: "none", lg: "block" },
+              display: { xs: 'none', lg: 'block' },
               width: 300,
               minWidth: 300,
             }}
           >
-            <Card sx={{ boxShadow: "none", borderRadius: "12px" }}>
-              <List sx={{ color: "#6e7b85" }}>
+            <Card sx={{ boxShadow: 'none', borderRadius: '12px' }}>
+              <List sx={{ color: '#6e7b85' }}>
                 <ListItem divider>
                   <Box
                     sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      width: "100%",
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      width: '100%',
                     }}
                   >
-                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
                       <Box sx={{ pr: 1 }}>
                         <PersonIcon />
                       </Box>
@@ -149,13 +149,13 @@ const ArticleDetail: NextPage = () => {
                 <ListItem divider>
                   <Box
                     sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      width: "100%",
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      width: '100%',
                     }}
                   >
-                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
                       <Box sx={{ pr: 1 }}>
                         <ArticleIcon />
                       </Box>
@@ -169,13 +169,13 @@ const ArticleDetail: NextPage = () => {
                 <ListItem>
                   <Box
                     sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      width: "100%",
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      width: '100%',
                     }}
                   >
-                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
                       <Box sx={{ pr: 1 }}>
                         <UpdateIcon />
                       </Box>
@@ -192,7 +192,7 @@ const ArticleDetail: NextPage = () => {
         </Box>
       </Container>
     </Box>
-  );
-};
+  )
+}
 
-export default ArticleDetail;
+export default ArticleDetail
