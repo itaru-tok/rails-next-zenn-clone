5fde25b094d50561c61c9295ff91892a00cbfe56
itaruby_on_rails <itaruo93o@icloud.com>
2023-10-15 19:16:14 +0900

    Confirmation page, user authentification API

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
       get "health_check", to: "health_check#index"
       mount_devise_token_auth_for "User", at: "auth"
       mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
+      namespace :user do
+        resource :confirmations, only: [:update]
+      end
       namespace :current do
         resource :user, only: [:show]
         resources :articles, only: [:create]
