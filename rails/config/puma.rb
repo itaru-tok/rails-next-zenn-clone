0d6078d5012a1ca1a13e6b38e3d0327db27a6018
itaruby_on_rails <itokuda@cuon.co.jp>
2023-11-22 10:48:16 +0900

    update puma

--- rails/config/puma.rb
+++ rails/config/puma.rb
@@ -1,14 +1,54 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
-environment ENV.fetch("RAILS_ENV") { "development" }
+# frozen_string_literal: true
+
+# setting for AWS
+# threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+# threads threads_count, threads_count
+# environment ENV.fetch("RAILS_ENV") { "development" }
+
+# # Specifies the `pidfile` that Puma will use.
+# pidfile ENV.fetch("PIDFILE") { "/myapp/tmp/pids/server.pid" }
+
+# plugin :tmp_restart
+
+# app_root = File.expand_path("..", __dir__)
+# bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# preload_app!
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+#
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port ENV.fetch("PORT", 3000)
+
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV", "development")
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "/myapp/tmp/pids/server.pid" }
+pidfile ENV.fetch("PIDFILE", "/myapp/tmp/pids/server.pid")
 
-plugin :tmp_restart
-
-app_root = File.expand_path("..", __dir__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
-
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked web server processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
 preload_app!
+
+# Allow puma to be restarted by `bin/rails restart` command.
+plugin :tmp_restart
