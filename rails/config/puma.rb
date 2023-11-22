b77b371a0bb28f737a1ab283c8b8196edeb75a9e
itaruby_on_rails <itokuda@cuon.co.jp>
2023-11-22 10:04:06 +0900

    add pidfile writing to puma.rb

--- rails/config/puma.rb
+++ rails/config/puma.rb
@@ -1,6 +1,10 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
 plugin :tmp_restart
 
 app_root = File.expand_path("..", __dir__)
